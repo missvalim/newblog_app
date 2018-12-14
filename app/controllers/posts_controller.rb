18fed750983b21840cd16da35fa4f035688915bf
missvalim <533masya7@mail.ru>
2018-12-14 18:05:18 +0500

    Initialize repository

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+	before_action :set_post, only: [ :show, :edit, :update, :destroy]
+	def index
+    @posts = Post.where("title LIKE ?","%#{params[:search]}%")
+		@posts = Post.paginate(page: params[:page], per_page: 5)
+  
+
+	end
+	def show
+
+    end
+  def new
+  	@post = Post.new 
+  end
+  
+  def create 
+  	@post = Post.new(post_params)
+  	if @post.save
+  		redirect_to @post, success: 'Статья успешно создана'
+  	else
+      flash.now[:danger] = 'Статья не создана'
+  		render :new
+    end
+   end
+  
+  def edit
+
+  end
+  
+  def update
+     if @post.update_attributes(post_params)
+     	redirect_to @post, success: 'Статья успешно обновлена'
+
+     else
+      flash.now[:danger] = 'Статья не обновлена'
+     	render :edit
+     end
+  end
+ 
+  def destroy
+  	@post.destroy
+  	redirect_to posts_path, success: 'Статья успешно удалена'
+  end
+
+  private
+  def set_post
+  	 @post = Post.find(params[:id])
+  end
+  def post_params
+  	params.require(:post).permit(:title, :body, :image, :all_tags)
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+ 
+ devise_for :users
+
+ root 'posts#index'
+ resources :posts
+ resources :pictures, only: [:create, :destroy]
+ resources :tags, only: [:show]
+ resources :categories
+ resources :searches
+end
